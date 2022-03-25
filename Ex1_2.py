d3c54d6cb88fd5718fc12fb4470ba0178e13ef80
shilux42 <84132680+shilux42@users.noreply.github.com>
2022-03-25 15:45:06 +0100

    Add files via upload

---
+++ Ex1_2.py
@@ -0,0 +1,21 @@
+"""Start the Python interpreter and use it as calculator.
+
+1. How many seconds are there in 42 minutes 42 seconds?
+2. How many miles are there in 10 kilometers? Hint: there are 1.61 kilometers in a mile.
+3. If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace (time per mile in minutes and seconds)?
+   What is your average speed in miles per hour?"""
+
+#1
+42 * 60 + 42 #2562 seconds in 42 minutes 42 seconds
+
+#2
+10 / 1.61 #6.2 miles in 10 kilometers
+
+#3
+2562 / 6.2 #413 seconds per mile
+413 / 60 #6.8 minutes per miles
+60 * 0.8 #48 | 6 minutes and 48 seconds per miles
+
+60 * 60 #3600 seconds in hour
+3600 / 413 #8.7 miles per hour
+
