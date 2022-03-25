be60199be32d4e8b8140fdc7f621d8b04771c9b3
shilux42 <84132680+shilux42@users.noreply.github.com>
2022-03-25 15:47:30 +0100

    Add files via upload

---
+++ Ex2_1.py
@@ -0,0 +1,17 @@
+"""Repeating my advice from the previous chapter, whenever you learn a new feaure, you should try it out in interactive mone
+and male errors on purpose to see what goes wrong"""
+
+#we've seen tha n = 42 is legal. What about 42 = n?
+# SyntaxError: cannot assign to literal
+
+#How about x = y = 1?
+#correct, assign the value 1 to x and y
+
+#In some languages every statement ends with a semi-colon, ;. What happens if you put a semi-colon at the end of a Python statement?
+#The statement is legal with a semi-colon at the end.
+
+#What if you put a period at the end of a statement?
+#A period at the end of a statement is a SyntaxError: invalid syntax
+
+#in math notation you can multiply x and y like this: xy. What happens if you try that in Python?
+#'xy' is not defined
