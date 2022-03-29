d278bb8fe68f86a15a1915c7af047749cf6a8943
shilux42 <84132680+shilux42@users.noreply.github.com>
2022-03-29 09:25:03 +0200

    Add files via upload

---
+++ Ex3_2.py
@@ -0,0 +1,53 @@
+"""A function object is a value you can assign to a variable or pass as an argument. 
+For example, do_twice is a function that takes a function object as an argument and calls it twice:
+
+def do_twice(f):
+    f()
+    f()
+
+Here’s an example that uses do_twice to call a function named print_spam twice.
+
+def print_spam():
+    print('spam')
+
+do_twice(print_spam)
+
+1. Type this example into a script and test it.
+
+2. Modify do_twice so that it takes two arguments, a function object and a value, and calls the
+function twice, passing the value as an argument.
+
+3. Copy the definition of print_twice from earlier in this chapter to your script.
+
+4. Use the modified version of do_twice to call print_twice twice, passing 'spam' as an
+argument.
+
+5. Define a new function called do_four that takes a function object and a value and calls the
+function four times, passing the value as a parameter. There should be only two statements in
+the body of this function, not four."""
+
+
+def doTwice(f, arg):
+    """Runs a function twice.
+
+    f: function object
+    arg: argument passed to the fuction"""
+    f(arg)
+    f(arg)
+
+def print_twice(arg):
+    """Prints the argument twice.
+    arg: anything printable"""
+    print(arg)
+    print(arg)
+
+def printSpam(arg):
+    print(arg)
+
+def doFour(f, arg):
+    doTwice(f, arg)
+    doTwice(f, arg)
+
+doTwice(print, 'spam')
+print('<---->')
+doFour(print, 'spam')
